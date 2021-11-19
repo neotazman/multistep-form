18196015e60d1478efd3d94510dba46644e82c50
Tristan Alie <TristanAlie@yahoo.com>
2021-11-19 16:18:13 -0500

    got the page switcher going

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -12,13 +12,55 @@ export default function UserForm() {
     }
 
     const[state, setState] = useState(initialState)
+    
 
-    const nextStep = () => setState( state.step++ )
-    const previousStep = () => setState( state.step-- )
+    const nextStep = () => {
+        console.log(state)
+        setState( { ...state, step: state.step + 1 })
+    }
+    const previousStep = () => {
+        setState( { ...state, step: state.step - 1 })
+    }
 
-    return (
-        <div>
-            the step is {state.step}
-        </div>
-    )
+    switch(state.step) {
+        case 1:
+          return (
+              <div>
+                <h1>home page</h1>
+                <button onClick={nextStep} className="navigate next">Next Step</button>
+              </div>
+              
+          ) 
+        case 2:
+          return (
+            <div>
+                <h1>page 2</h1>
+                <button onClick={previousStep} className="navigate previous">Previous Step</button>
+                <button onClick={nextStep} className="navigate next">Next Step</button>
+            </div>
+          ) 
+        case 3:
+          return (
+            <div>
+                <h1>page 3</h1>
+                <button onClick={previousStep} className="navigate previous">Previous Step</button>
+                <button onClick={nextStep} className="navigate next">Next Step</button>
+            </div>
+          ) 
+        case 4:
+          return (
+            <div>
+                <h1>final page</h1>
+                <button onClick={previousStep} className="navigate previous">Previous Step</button>
+            </div>
+          ) 
+      }
+
+    // return (
+    //     <div>
+    //         <h3>the step is {state.step}</h3>
+    //         <button onClick={previousStep} className="navigate previous">Previous Step</button>
+    //         <button onClick={nextStep} className="navigate next">Next Step</button>
+    //     </div>
+    // )
 }
