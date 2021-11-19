fcd921944093f571c6fd66ca02d79c06da35e129
Tristan Alie <TristanAlie@yahoo.com>
2021-11-19 14:01:59 -0500

    created initial state and imported useState

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function UserForm() {
-    
+    const initialState = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: '',
+    }
+
+    const[state, setState] = useState(initialState)
+
     return (
         <div>
             
